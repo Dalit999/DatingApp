1095fe14e879dbe6d22e4582832006a3b9d0013b
Dalit999 <36208218+Dalit999@users.noreply.github.com>
2020-05-02 23:09:12 +0300

    Middle of chapter 12

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import {TabsModule} from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
@@ -51,10 +52,12 @@ export function tokenGetter()
    ],
    imports: [
       BrowserModule,
+      BrowserAnimationsModule,
       HttpClientModule,
       FormsModule,
-      BrowserAnimationsModule,
+      ReactiveFormsModule,
       BsDropdownModule.forRoot(),
+      BsDatepickerModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
       FileUploadModule,
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit ,Input, Output, EventEmitter} from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyjsService } from '../_services/alertifyjs.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/ngx-bootstrap-datepicker';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +12,48 @@ import { AlertifyjsService } from '../_services/alertifyjs.service';
 export class RegisterComponent implements OnInit {
   //@Input() valuesFromHome:any;
   @Output() cancelRegister = new EventEmitter();
-model: any={};
-  constructor(private authService:AuthService, private alertifyjs:AlertifyjsService) { }
+  model: any={};
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDatepickerConfig>;
+  constructor(private authService:AuthService, private alertifyjs:AlertifyjsService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    // this.registerForm = new FormGroup({
+    //   username: new FormControl('', Validators.required),
+    //   password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('',  Validators.required),
+    // },
+    // this.passwordMatchValidator);
+    this.bsConfig = {
+      containerClass:'theme-red'
+    };
+    this.createRegisterForm();
   }
-
+createRegisterForm(){
+  this.registerForm = this.fb.group({
+    gender: ['female'],
+    username: ['', Validators.required],
+    knownAs: ['', Validators.required],
+    dateOfBirth: [null, Validators.required],
+    city: ['', Validators.required],
+    country: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+    confirmPassword: ['',  Validators.required]
+  }, {validator: this.passwordMatchValidator});
+}
+  passwordMatchValidator(g: FormGroup){
+    return g.get('password').value === g.get('confirmPassword').value?
+    null :
+    {'mismatch':true};
+  }
   register()
   {
-    this.authService.register(this.model).subscribe(
-      ()=>this.alertifyjs.success('register successful'),
-      (error)=>this.alertifyjs.error(error) 
-    )
+    // this.authService.register(this.model).subscribe(
+    //   ()=>this.alertifyjs.success('register successful'),
+    //   (error)=>this.alertifyjs.error(error) 
+    // );
+    console.log(this.registerForm.value);
   }
   cancel()
   {
